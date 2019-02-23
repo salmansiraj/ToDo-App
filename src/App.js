7ad7e36186a5853e66708e700864b4e3c317b0f5
Salman Siraj <ss10298@nyu.edu>
2019-02-23 13:11:09 -0500

    first draft

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ToDo App</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
